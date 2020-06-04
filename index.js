53ad3e90528f0d95765872ed64d3ec2bde4c9433
Chandrakanha Dharmik <66319279+Kanhadharmik@users.noreply.github.com>
2020-06-05 00:26:57 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,9 @@
+
+$(".slider").click(function(){
+
+  $("body").toggleClass("light-section");
+  $("header").toggleClass("light-header");
+  $(".boxes").toggleClass("light-boxes");
+  $(".details").toggleClass("light-boxes");
+  $(".numbers").toggleClass("light-numbers");
+});
